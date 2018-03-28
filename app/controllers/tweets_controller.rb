46327416edad818744911268f75cae217bbb319e
Caro Evans <caroline.w.evans@gmail.com>
2018-03-28 11:39:43 +1000

    changes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
-
+  before_action :check_owner, only: [:edit, :update, :destroy]
   # GET /tweets
   # GET /tweets.json
   def index
@@ -70,5 +70,11 @@ class TweetsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def tweet_params
       params.require(:tweet).permit(:content, :user_id)
+    end
+
+    def  check_owner
+      if current_user != @tweet.user
+      redirect_to tweets_url, notice: 'You are not authorized to perform that action.'
+      end
     end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,4 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+  validates :content, presence: { message: 'Please enter some content!'}
 end
